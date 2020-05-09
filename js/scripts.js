15c57691c1b0dd3cd0377a19c1b9c647d85446de
Taymour Khan Niazi <30389323+taymourniazi@users.noreply.github.com>
2020-05-09 18:27:58 +0400

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+var callback = function(){
+	$('.item-skills').each(function(){
+		newWidth = $(this).parent().width() * $(this).data('percent');
+		$(this).width(0);
+    $(this).animate({
+        width: newWidth,
+    }, 4000);
+	});
+	$('.icons-red').each(function(){
+		height = $(this).height();
+    $(this).animate({
+        height: 14,
+    }, 30000);
+	});
+};
+$(document).ready(callback);
+
+var resize;
+window.onresize = function() {
+	clearTimeout(resize);
+	resize = setTimeout(function(){
+		callback();
+	}, 100);
+};
